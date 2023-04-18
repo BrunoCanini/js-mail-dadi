1c5322ac364aa3b6c1fec2ad299b0593c1803a01
Bruno Canini <brunocanini1994@gmail.com>
2023-04-18 17:02:11 +0200

    in progress

--- main.js
+++ main.js
@@ -7,23 +7,20 @@
 // VERO (ANDIAMO AVANTI)
 // FALSO (ERRORE)
 
-// const emailUtente = document.getElementById("email");
-
-let emailValide = ["bruno@boolean.com" , "tizio@boolean.com" , "caio@boolean.com"];
-
 const button = document.getElementById("bottone");
 
 button.addEventListener( "click" , function() {
     
-    let emailUtente = document.getElementById("email");
+    let emailUtente = document.getElementById("email").value;
+    let emailValide = ["bruno@boolean.com" , "tizio@boolean.com" , "caio@boolean.com"];
 
     for (let c = 0; c < emailValide.length; c++) {
-        const validaEmail = emailValide[c];
+        let validaEmail = emailValide[c];
 
         if(emailUtente == validaEmail){
-            allert("benvenuto");
-        } else if(emailUtente != validaEmail){
-            allert("Non sei autorizzato");
+            alert("benvenuto");
+        } else{
+            alert("Non sei autorizzato");
         }
         
     }
